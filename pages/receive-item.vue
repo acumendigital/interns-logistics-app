1b519ccbb650d3c9b72c299183e602cfff734062
Harith Onigemo <45686269+Harithmetic1@users.noreply.github.com>
2022-05-25 16:30:29 +0100

    :sparkles: Added design changes

--- pages/receive-item.vue
+++ pages/receive-item.vue
@@ -1,20 +1,34 @@
 <template>
   <div class="receive-item-container">
     <div class="close-btn">
-      <img src="~/assets/images/close.svg" alt="close">
+      <nuxt-link to="/home"> 
+        <img src="~/assets/images/close.svg" alt="close">
+      </nuxt-link>
     </div>
     <div class="page-indicators">
       <div class="send">
-        <p>Send</p>
+        <nuxt-link to="">
+          Send
+        </nuxt-link>
       </div>
-      <div class="receive">
-        <p>Receive</p>
+      <div class="receive active">
+        <nuxt-link to="">
+          Receive
+        </nuxt-link>
       </div>
     </div>
 
     <form action="">
       <div class="pick-up-location">
-        <label for="pick-up">Pick-up Location</label>
+        <div class="select-state-selector">
+          <label for="pick-up">Pick-up Location</label>
+          <div class="select-state">
+            <select name="state" id="state">
+              <option value="state-1">Select state</option>
+            </select>
+          </div>
+        </div>
+        
         <div class="input-entry">
           <img src="~/assets/images/location.svg" alt="location">
           <input type="text" id="pick-up" placeholder="Enter Location">
@@ -25,6 +39,21 @@
         <div class="input-entry">
           <img src="~/assets/images/current-location.svg" alt="current location">
           <input type="text" id="drop-off" placeholder="Current Location">
+        </div>
+      </div>
+      <div class="contact-information-container">
+        <div class="container-header">
+          <h2>
+            Contact Information
+          </h2>
+        </div>
+        <div class="contact-information">
+          <div class="phone-number">
+            <input type="number" name="phone-number" id="phone-number" placeholder="Phone Number">
+          </div>
+          <div class="email-address">
+            <input type="email" name="email-address" id="email-address" placeholder="Email Address">
+          </div>
         </div>
       </div>
       <div class="time-and-date-container">
@@ -82,6 +111,21 @@ export default {};
       display: flex;
       align-items: center;
       gap: 8px;
+      a{
+        text-decoration: none;
+        color: #B0B0B0;
+      }
+      .active{
+        background: #FFD60A;
+        border-radius: 8px;
+        padding: 8px;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 28px;
+        a{
+          color: black;
+        }
+      }
       .receive{
         @include active-btn;
       }
@@ -89,6 +133,10 @@ export default {};
     form{
       padding-top: 32px;
       .pick-up-location{
+        .select-state{
+          @include select-field;
+          margin-bottom: 12px;
+        }
         display: flex;
         flex-direction: column;
         gap: 8px;
@@ -113,6 +161,31 @@ export default {};
         }
         .input-entry{
           @include input-field;
+        }
+      }
+      .contact-information-container{
+        .container-header{
+          margin-bottom: 32px;
+          h2{
+            font-weight: 400;
+            font-size: 18px;
+            line-height: 28px;
+            color: #575757;
+          }
+        }
+        .contact-information{
+          // display: flex;
+          // align-items: center;
+          // flex-wrap: wrap;
+          // gap: 32px;
+          .phone-number{
+            @include input-field;
+            margin-bottom: 32px;
+          }
+          .email-address{
+            @include input-field;
+            margin-bottom: 48px;
+          }
         }
       }
       .time-and-date-container{
@@ -143,12 +216,7 @@ export default {};
           }
         }
         .package-description{
-          @include input-field;
-          select{
-            width: 100%;
-            background: transparent;
-            border: none;
-          }
+          @include select-field;
         }
         .more-description{
           @include input-field;
@@ -165,6 +233,11 @@ export default {};
           border: none;
         } 
       }
+    }
+  }
+  @media (min-width: 1099px){
+    .receive-item-container{
+      padding: 32px 30vw;
     }
   }
 </style>
