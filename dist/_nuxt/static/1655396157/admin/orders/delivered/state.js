a7a1bee7f90c219158c2048b6d293a499dbacce1
Harith Onigemo <onigemotosin@gmail.com>
2022-06-16 17:40:17 +0100

    :lipstick: updating splash screen UI

--- dist/_nuxt/static/1655396157/admin/orders/delivered/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655396157",layout:"AdminLayout",error:a,state:{userDetails:[],loading:false,token:""},serverRendered:true,routePath:"\u002Fadmin\u002Forders\u002Fdelivered",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- dist/_nuxt/static/1655396157/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655396157",layout:"default",error:a,state:{userDetails:[],loading:false,token:""},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
